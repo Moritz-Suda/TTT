d6af85902e613f1527f8efc0a603a28913fd8095
M <120421567+Moritz-Suda@users.noreply.github.com>
2025-02-07 13:18:43 +0000

    'fixed' check_full

--- check_full.py
+++ check_full.py
@@ -1,4 +1,12 @@
 def check_full(board,a1_captured, a2_captured, a3_captured, b1_captured, b2_captured, b3_captured, c1_captured, c2_captured, c3_captured):
-    if a1_captured == True and a2_captured == True and a3_captured == True and  b1_captured == True and  b2_captured == True and b3_captured == True and c1_captured == True and  c2_captured == True and c3_captured == True:
-        print("You've fucked up!")
+    if a1_captured is True
+        if a2_captured is True
+            if a3_captured is True
+                if b1_captured is Ture
+                    if b2_captured is True
+                        if b3_captured is True
+                            if c1_captured is True
+                                if c2_captured is True
+                                    if c3_captured is True
+                                        print("You've fucked up!")
     
