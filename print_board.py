4f59f7f6335f3fdfa39888973db28de76a9c3147
Shad0w27 <shad0w27.66242007@gmail.com>
2025-02-09 14:41:31 +0100

    added consoleClear for better readability

--- print_board.py
+++ print_board.py
@@ -1,9 +1,18 @@
+import os
+
+
 # Prints the board
 def print_board(board):
-    print()
+    clearConsole()
     for i in range(0, 8, 3):
         print(board[i], "|", board[i+1], "|", board[i+2])
         if (i == 0 or i == 3):
-            print("-------------")
+            print("---------")
     print()
 
+
+def clearConsole():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
